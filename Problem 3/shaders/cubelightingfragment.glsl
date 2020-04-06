02d68a3ef188c8c0be760155585ea0400b0ff233
geekpradd <geekpradd@gmail.com>
2020-04-06 22:41:23 +0530

    update

---
+++ Problem 3/shaders/cubelightingfragment.glsl
@@ -0,0 +1,40 @@
+#version 330 core
+out vec4 output;
+in vec3 FragPos;
+in vec3 normal;
+in vec2 texCoords;
+
+struct Material {
+    sampler2D TEXTURE;
+    vec3 specular;
+    float shininess;
+};
+
+struct Light {
+    vec3 position;
+    vec3 ambient;
+    vec3 specular;
+    vec3 diffuse;
+};
+
+uniform Material material;
+uniform Light light;
+uniform mat4 normalMatrix;
+uniform vec3 viewLoc;
+
+void main(){
+    vec3 ambient = light.ambient * vec3(texture(material.TEXTURE, texCoords));
+
+    vec3 normal_n = mat3(normalMatrix) * normalize(normal);
+    vec3 lightDir = normalize(light.position - FragPos);
+    vec3 reflectDir = reflect(-lightDir, normal_n);
+    vec3 viewDir = normalize(viewLoc - FragPos);
+
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+    vec3 specular =  spec * material.specular * light.specular;
+    float diffuseStrength = max(dot(lightDir, normal_n), 0.0);
+    vec3 diffuse = diffuseStrength * vec3(texture(material.TEXTURE, texCoords)) * light.diffuse;
+
+    vec3 Color = ambient + diffuse + specular;
+    output = vec4(Color, 1.0f);   
+}   
